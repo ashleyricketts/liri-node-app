2fd959fe6d7d7cf6de3843919114b6bed7127efb
Ashley Ricketts <aricketts12@gmail.com>
2019-04-01 20:28:46 -0500

    writes results to file

--- liri.js
+++ liri.js
@@ -4,9 +4,13 @@ var keys = require("./key.js");
 
 var axios = require("axios");
 
+var fs = require("fs");
+
 var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
+
+var moment = require('moment');
 
 var nodeArgs = process.argv;
 
@@ -18,34 +22,46 @@ var lineBreak = "\n----------------------------------------------\n";
 
 
 //user input for search in api's
-for(var i = 3; i < nodeArgs.length; i++){
-    if(i > 3 && i < nodeArgs.length){
-      search = search + '+' + nodeArgs[i];
-    } 
-    
-    else{
-      search += nodeArgs[i];
+for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+        search = search + '+' + nodeArgs[i];
     }
-  };
+
+    else {
+        search += nodeArgs[i];
+    }
+};
 
 
 
-function liri(){
+function liri() {
 
     //function to run BandsInTown search
-    if(action === "concert-this"){
+    if (action === "concert-this") {
         axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp")
 
             .then(
-                function(response) {
-                    for (i = 0; i < response.data.length; i++){
-                        console.log(lineBreak + 
-                        "Venue: " + response.data[i].venue.name +
-                        "\nLocation: " + response.data[i].venue.city + ", " + response.data[i].venue.country +
-                        "\nDate: " + response.data[i].datetime + lineBreak);
+                function (response) {
+                    for (i = 0; i < response.data.length; i++) {
+                        console.log(lineBreak +
+                            "Venue: " + response.data[i].venue.name +
+                            "\nLocation: " + response.data[i].venue.city + ", " + response.data[i].venue.country +
+                            "\nDate: " + moment(response.data[i].datetime).format("MM/DD/YYYY") + lineBreak);
+
+                        var logArtist =
+                            lineBreak +
+                            "Venue: " + response.data[i].venue.name +
+                            "\nLocation: " + response.data[i].venue.city + ", " + response.data[i].venue.country +
+                            "\nDate: " + moment(response.data[i].datetime).format("MM/DD/YYYY") + lineBreak;
+
+                        fs.appendFile("log.txt", logArtist, function (err) {
+                            if (err) {
+                                return console.log(err);
+                            }
+                        })
                     }
                 }
-            )   
+            )
     }
 
     //function to run OMDB search
@@ -53,7 +69,7 @@ function liri(){
         axios.get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy")
 
             .then(
-                function(response) {
+                function (response) {
                     console.log(lineBreak +
                         "Title: " + response.data.Title +
                         "\nYear: " + response.data.Year +
@@ -63,28 +79,58 @@ function liri(){
                         "\nLanguage: " + response.data.Language +
                         "\nPlot: " + response.data.Plot +
                         "\nActors: " + response.data.Actors + lineBreak);
-                }
-            );
+
+                    var logMovie =
+                        lineBreak +
+                        "Title: " + response.data.Title +
+                        "\nYear: " + response.data.Year +
+                        "\nIMDB Rating: " + response.data.imdbRating +
+                        "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                        "\nCountry: " + response.data.Country +
+                        "\nLanguage: " + response.data.Language +
+                        "\nPlot: " + response.data.Plot +
+                        "\nActors: " + response.data.Actors + lineBreak;
+
+                    fs.appendFile("log.txt", logMovie, function (err) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                    }
+                    );
+                })
     }
 
     //function to run Spotify search
-    if (action === "spotify-this-song"){
+    if (action === "spotify-this-song") {
         spotify
-            .search({ type: 'track', query: search})
-            .then(function(data) {
+            .search({ type: 'track', query: search })
+            .then(function (data) {
                 console.log(lineBreak +
                     "Artist: " + data.tracks.items[0].artists[0].name +
                     "\nTrack: " + data.tracks.items[0].name +
-                    "\nPreview Link: " + data.tracks.items[0].preview_url + 
+                    "\nPreview Link: " + data.tracks.items[0].href +
                     "\nAlbum Name: " + data.tracks.items[0].album.name + lineBreak);
+
+                var logSong =
+                    lineBreak +
+                    "Artist: " + data.tracks.items[0].artists[0].name +
+                    "\nTrack: " + data.tracks.items[0].name +
+                    "\nPreview Link: " + data.tracks.items[0].href +
+                    "\nAlbum Name: " + data.tracks.items[0].album.name + lineBreak;
+
+                fs.appendFile("log.txt", logSong, function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
             })
-                .catch(function(err) {
-                    console.log(err);
-                });
     }
 
 
-    else{
+    else {
         console.log("Please choose an action.");
     }
 };
